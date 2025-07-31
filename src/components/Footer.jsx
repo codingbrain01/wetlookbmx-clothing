b631bd44947202a40d7ee8d2f86cd5516e790e03
codingbrain <codingbrain01@gmail.com>
2025-07-31 13:44:05 +0800

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-black text-white p-4 text-center">
-      <p>&copy; Nedmac 2025 - WetLook BMX Clothing</p>
-    </footer>
-  );
-}
-
-export default Footer;
